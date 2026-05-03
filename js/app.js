c30f09c5a2d32ad11ee98bf344c6388a3384f929
jminbae <jminbae@gmail.com>
2026-05-03 23:04:58 +0900

    메타 타이틀 수정: '피부과 전문의가 전하는 예뻐지는 모든 이야기'

--- js/app.js
+++ js/app.js
@@ -933,7 +933,7 @@
       const hero = $('#hero');
       if (hero) hero.style.display = '';
       // 메타 복원
-      document.title = '피부텐텐 Q&A. 피부과 전문의가 전하는 피부가 예뻐지는 모든 이야기';
+      document.title = '피부텐텐 Q&A. 피부과 전문의가 전하는 예뻐지는 모든 이야기';
       renderResults(q);
     }
     highlightChips(q);
